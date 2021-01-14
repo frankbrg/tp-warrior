56691f6c00033f531072b2079160714ca524268e
Frank Bergé <frank.brg@icloud.com>
2021-01-14 18:08:54 +0100

    :sparkles: add weapon

--- tp-warrior/src/App.java
+++ tp-warrior/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) {
 
-       Guerrier conan = new Guerrier("Conan");
+        Guerrier conan = new Guerrier("Conan");
 
-       conan.taper(); // Affiche Je suis Conan et je tape comme une buse
+        conan.taper(); // Affiche Je suis Conan et je tape comme une buse
 
         conan.levelUp();
 
@@ -17,14 +17,32 @@ public class App {
 
         conan.taper("Berserk attaque"); //  Afficher Je suis Conan et je tape avec mon attaque sépciale BERSERK ATTACK !!!.
 
-
+        conan.setWeapon(new Epee());
+        
+        conan.taper(); // Je suis conan et erùlkgsrùkhlnùr, je tape avec mon epée
+    
+        conan.setWeapon(new Maudite(conan.getWeapon()));
+    
+        conan.taper(); // Je suis conan et erùlkgsrùkhlnùr, je tape avec mon epée maudite
+    
+        conan.setWeapon(new Enflamme(conan.getWeapon()));
+    
+        conan.taper(); // Je suis conan et erùlkgsrùkhlnùr, je tape avec mon epée maudite enflammée
+    
+        conan.setWeapon(new QuiBrille(conan.getWeapon()));
+    
+        conan.taper(); // Je suis conan et erùlkgsrùkhlnùr, je tape avec mon epée maudite enflammée qui brille
+    
+        conan.levelUp();
+    
+        conan.taper(); // Je suis conan et truc différent, je tape avec mon epée maudite enflammée qui brille
 
         Guerrier shera = new Guerrier("Shera");
-
-        shera.levelUp();
-
-        shera.taper(); // Afficher Je suis Shera et je tape en faisant des moulinés stylés avec mon épée.
-
+        
+        shera.setWeapon(new Maudite(new Hache()));
+    
+        shera.taper(); // Je suis Shera et erùlkgsrùkhlnùr, je tape avec mon hache maudite
+        }
     }
 
 }
